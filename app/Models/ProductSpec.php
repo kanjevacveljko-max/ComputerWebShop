f87f6535a61ca91096e6e8d5c57d4af5359f03b8
Veljko <kanjevacveljko@gmail.com>
2025-12-26 03:26:27 +0100

    Created ProductSpecs model and migration

---
+++ app/Models/ProductSpec.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductSpec extends Model
+{
+    protected $table = 'product_spec';
+
+    protected $fillable = ['product_id', 'spec_name', 'spec_value'];
+}
